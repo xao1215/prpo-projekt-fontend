ecfeb0c949e0f21109767110d2c2d94527cb701d
xao1215 <allexxpez@gmail.com>
2022-02-11 21:40:29 +0100

    stili

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,11 +4,11 @@ import {Component} from '@angular/core';
     moduleId: module.id,
     selector: 'prpo-app',
     template: `
-        <h1 class="text-warning">{{naslov}}</h1>
+        <h1 class="text-dark fw-bold">{{naslov}}</h1>
         <router-outlet></router-outlet>
     `,
     styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-    naslov = 'Polnilne postaje';
+    naslov = 'Polnilne Postaje';
 }
